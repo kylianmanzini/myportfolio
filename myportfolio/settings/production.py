4a21968193f5f4859439f30d355a0ac3e78ada75
Kylian <37073277+Kykiman@users.noreply.github.com>
2018-09-07 15:22:12 +0100

    r6

--- myportfolio/settings/production.py
+++ myportfolio/settings/production.py
@@ -4,6 +4,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['kylianmanzini.pythonanywhere.com'] 
 
+SECRET_KEY = '01xxb#e2)ubqvwyip9hu0xi6(*(rla8)s)5#663u@oj^eg3q2q'
+
 try:
     from .local import *
 except ImportError:
